12cd781922e42c52ae0bdf8a9669e2e16a84ee42
zehaeva <zehaeva@gmail.com>
2015-04-28 17:50:45 -0400

    Small updates Added empty_cell varible have commented out some debug code Condensed cell checking algo

--- .idea/main.py
+++ .idea/main.py
@@ -5,7 +5,7 @@ class SudokuBoard:
     size = 9
     sub = 3
     allow_invalid = True
-
+    empty_cell = '_'
     #board = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [9, 1, 2, 3, 4, 5, 6, 7, 8], [8, 9, 1, 2, 3, 4, 5, 6, 7],
     #         [7, 8, 9, 1, 2, 3, 4, 5, 6], [6, 7, 8, 9, 1, 2, 3, 4, 5], [5, 6, 7, 8, 9, 1, 2, 3, 4],
     #         [4, 5, 6, 7, 8, 9, 1, 2, 3], [3, 4, 5, 6, 7, 8, 9, 1, 2], [2, 3, 4, 5, 6, 7, 8, 9, 2]]
@@ -13,12 +13,12 @@ class SudokuBoard:
     def __init__(self, size):
         self.size = size
         self.sub = int(size ** (1/2))
-        self.board = [['_' for x in range(1, size + 1)] for x in range(1, size + 1)]
+        self.board = [[self.empty_cell for x in range(1, size + 1)] for x in range(1, size + 1)]
 
     def __repr__(self):
         my_return = ''
         for x in range(0, self.size):
-            my_return += ' '.join([str(x) for x in self.board[x]]) + '\n'
+            my_return += ' '.join([str(y) for y in self.board[x]]) + '\n'
         return(my_return)
 
     def set_cell(self, value, x, y):
@@ -30,6 +30,7 @@ class SudokuBoard:
                 raise
 
     def valid_cell(self, x, y):
+        #print('Check Cell [{}][{}]: {}'.format(x, y, self.board[x][y]))
         am_i = True
         for z in range(0, self.size):
             # Rows
@@ -38,28 +39,30 @@ class SudokuBoard:
                 break
             # Columns
             if self.board[x][y] == self.board[z][y] and z != x:
+                #print('{} ?= {}'.format(self.board[x][y], self.board[z][y] ))
                 am_i = False
                 break
-        # now to check the subcell
-        # which cell_row am i in?
-        cell_row = int(int(x / self.sub) * self.sub)
-        cell_column = int(int(y / self.sub) * self.sub)
 
-        for xs in range(cell_row, (cell_row + self.sub)):
-            for ys in range(cell_column, (cell_column + self.sub)):
-                if self.board[x][y] == self.board[xs][ys] and (xs, ys) != (x, y):
-                    am_i = False
+        if not am_i:
+            # now to check the subcell
+            # which cell_row am i in?
+            cell_row = int(int(x / self.sub) * self.sub)
+            cell_column = int(int(y / self.sub) * self.sub)
+
+            for xs in range(cell_row, (cell_row + self.sub)):
+                for ys in range(cell_column, (cell_column + self.sub)):
+                    if self.board[x][y] == self.board[xs][ys] and (xs, ys) != (x, y):
+                        am_i = False
+                        break
+                if not am_i:
                     break
-            if not am_i:
-                break
         return am_i
 
     def valid_board(self):
-
         am_i = True
         for x in range(0, self.size):
             for y in range(0, self.size):
-                if self.board[x][y] is not None:
+                if self.board[x][y] != self.empty_cell:
                     am_i = self.valid_cell(x, y)
                     if not am_i:
                         break
@@ -78,7 +81,7 @@ class SudokuShell(cmd.Cmd):
         self.board = SudokuBoard(int(arg))
     def do_validate(self, arg):
         'Valid the state of the board'
-        if self.board.valid_board:
+        if self.board.valid_board():
             print('The board is valid!')
         else:
             print('Sorry! Something is wrong!')
@@ -89,7 +92,7 @@ class SudokuShell(cmd.Cmd):
     def do_show(self, arg):
         'Show the game board'
         print(self.board)
-    def do_bye(self, arg):
+    def do_exit(self, arg):
         'Stop recording, close the sudoku window, and exit:  BYE'
         print('Thank you for playing Sudoku')
         return True
@@ -97,7 +100,7 @@ class SudokuShell(cmd.Cmd):
 def parse(arg):
     'Convert a series of zero or more numbers to an argument tuple'
     return tuple(map(int, arg.split()))
-
+#'''
 if __name__ == '__main__':
     SudokuShell().cmdloop()
 '''
@@ -118,6 +121,8 @@ myboard.board[0][6] = 7
 myboard.board[0][7] = 9
 myboard.set_cell(9, 0, 8)#.board[3][8] = 9
 
+myboard.set_cell(1, 0, 1)
+myboard.set_cell(1, 0, 0)
 
 if myboard.valid_board():
     print('Valid')
